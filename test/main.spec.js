501896b28c865b0ca136fceac2562f6810e8b9fc
Octowl <alsaff1987@gmail.com>
2015-08-27 11:16:31 +0300

    Write dummy test

---
+++ test/main.spec.js
@@ -0,0 +1,11 @@
+/*global describe, it, chai*/
+
+(function () {
+  'use strict';
+
+  describe("The AddressBook App", function () {
+    it("should work", function () {
+      chai.assert.isArray([]);
+    });
+  });
+}());
