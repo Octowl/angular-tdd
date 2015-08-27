d0b4c8defebd1e6b43790185fe6a7b8c9abad35b
Octowl <alsaff1987@gmail.com>
2015-08-27 14:28:13 +0300

    Add backend request

--- app/main.js
+++ app/main.js
@@ -6,5 +6,8 @@
   angular.module('AddressBook', [])
     .service('contactService', function ($http) {
       this.contacts = [];
+      $http.get("http://localhost:9001/contacts", function (response) {
+        console.log(response);
+      });
     });
 }());
