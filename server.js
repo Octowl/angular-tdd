4db4ec60489a0efaf84c3acfbe5ba7b0a2da3de4
Octowl <alsaff1987@gmail.com>
2015-08-27 14:01:02 +0300

    Build simple express backend

---
+++ server.js
@@ -0,0 +1,19 @@
+/*global require*/
+
+(function () {
+  'use strict';
+
+  var express = require('express'),
+    app = express(),
+    contacts = [
+      {
+        name: "Dr. Patatis"
+      }, {
+        name: "Mrs. Patatis"
+      }];
+
+  app.get('/contacts', function (request, response) {
+    response.status(200).json(contacts);
+  });
+
+}());
