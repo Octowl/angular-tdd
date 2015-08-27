2c2ecf00d8ec0e1fefc50d17d74aad77d4702bbb
Octowl <alsaff1987@gmail.com>
2015-08-27 12:10:32 +0300

    Add karma configuration

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,18 @@
 (function () {
   'use strict';
   
-  module.exports = function () {
-    
+  module.exports = function (config) {
+    config.set({
+      browsers: ['PhantomJS'],
+      frameworks: ['mocha'],
+      files: [
+        "bower_components/angular/angular.js",
+        "bower_components/chai/chai.js",
+        
+        "app/**/*.js",
+        
+        "test/*.js"
+      ]
+    });
   };
 }());
