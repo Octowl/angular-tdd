e101cc889f1476b176500c80ab4e836865c9b8a9
Octowl <alsaff1987@gmail.com>
2015-08-27 11:08:10 +0300

    Add serve-test task

--- gulpfile.js
+++ gulpfile.js
@@ -20,4 +20,20 @@
     gulp.watch(['app/**/*.*'])
       .on('change', browserSync.reload);
   });
+  
+  gulp.task('serve-test', function () {
+    browserSync.init({
+      notify: false,
+      port: 8081,
+      server: {
+        baseDir: ["test", "app"],
+        routes: {
+          '/bower_components': 'bower_components'
+        }
+      }
+    });
+
+    gulp.watch(['app/**/*.*'])
+      .on('change', browserSync.reload);
+  });
 }());
