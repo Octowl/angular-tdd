6ec3c3bc04827e5be89cca3cebb9f6f40cf904d7
Octowl <alsaff1987@gmail.com>
2015-08-27 15:06:09 +0300

    Replace dummy test with test for contactService containing contacts array

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,11 +1,22 @@
-/*global describe, it, chai*/
+/*global describe, it, chai, module, inject*/
 
 (function () {
   'use strict';
 
+  var assert = chai.assert,
+    expect = chai.expect,
+    contactService;
+
   describe("The AddressBook App", function () {
-    it("should work", function () {
-      chai.assert.isArray([]);
+    describe("The contact service", function () {
+      it("has an array called contacts", function () {
+        module('AddressBook');
+        inject(function ($injector) {
+          contactService = $injector.get('contactService');
+        });
+
+        expect(contactService.contacts).to.be.an('array');
+      });
     });
   });
 }());
